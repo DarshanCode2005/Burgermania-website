21b9aa3b531abcb890e4f358a8280c2ca373d3aa
DarshanCode2005 <darshanthakare05@gmail.com>
2024-06-22 21:23:35 +0530

    Added the main section. Learnt a lot

--- script.js
+++ script.js
@@ -16,3 +16,23 @@ if(navClose){
         navMenu.classList.remove('show-menu')
     })
 }
+
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkActive(){
+    navMenu.classList.remove('show-menu')
+}
+
+navLink.forEach((e) => {
+    e.addEventListener('click',linkActive)
+})
+
+function scrollHeader(){
+    const header = document.getElementById('header')
+    // this
+    this.scrollY >= 50 ? header.classList.add('scroll-header'):
+                        header.classList.remove('scroll-header')
+                    
+}
+
+window.addEventListener('scroll',scrollHeader)
