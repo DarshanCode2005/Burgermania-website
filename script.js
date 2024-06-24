eb00bc46c158f94261f0ff12fce3ff67b066ad97
DarshanCode2005 <darshanthakare05@gmail.com>
2024-06-24 15:02:42 +0530

    Animation added, responsive breakpoints added, some minor work remaining

--- script.js
+++ script.js
@@ -37,3 +37,54 @@ function scrollHeader(){
 
 window.addEventListener('scroll',scrollHeader)
 
+
+function scrollUp(){
+    const scrollbutton = document.getElementById('scroll-up')
+
+    this.scrollY >=350 ? scrollbutton.classList.add('show-scroll'):
+                         scrollbutton.classList.remove('show-scroll')
+}
+
+const sections = document.querySelectorAll('section[id]')
+
+
+const scrollActive = () => {
+    const scrollDown = window.scrollY
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight,
+        sectionTop = current.offsetTop - 58,
+        sectionId = current.getAttribute('id'),
+        sectionClass = document.querySelector('.nav__menu a[href*='+sectionId+']')
+
+        if(scrollDown>sectionTop && scrollDown <= sectionTop+sectionHeight){
+            sectionClass.classList.add('active-link')
+        }
+        else{
+            sectionClass.classList.remove('active-link')
+        }
+    })
+    // complex
+}
+
+window.addEventListener('scroll',scrollActive)
+
+window.addEventListener('scroll',scrollUp)
+
+
+const sr = ScrollReveal(
+    {
+        origin:'top',
+        distance:'60px',
+        duration:2500,
+        delay:300
+    }
+)
+
+sr.reveal(`.home__data,.footer`)
+sr.reveal(`.home__dish`,{delay:500,distance:'100px',origin:'bottom'})
+sr.reveal(`.home__burger`,{delay:1200,distance:'100px',duration:1500})
+sr.reveal(`.home__ingredient`,{delay:1600,interval:100})
+sr.reveal(`.recipe__img,.delivery__img,.contact__image`,{origin:'left'})
+sr.reveal(`.recipe__data,.delivery__data,contact__data`,{origin:'right'})
+sr.reveal(`.popular__card`,{interval:100})
